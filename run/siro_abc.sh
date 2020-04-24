4c290975a6641a8c78981768f2e7eba805e7f1bc
changhoonhahn <changh20@gmail.com>
2020-04-23 21:11:36 -0400

    running SFR=0 presampled for simba on sirocco

--- run/siro_abc.sh
+++ run/siro_abc.sh
@@ -1,23 +1,26 @@
 # !/bin/bash
 #PBS -l nodes=1:ppn=24
-#PBS -N sim_slabnollm.L2.3d
+#PBS -N simba_slabnollmsfr.L2.3d
 cd $PBS_O_WORKDIR
 export NPROCS=`wc -l $PBS_NODEFILE |gawk '//{print $1}'`
 export PATH="/home/users/hahn/anaconda3/bin:$PATH"
 
 source /home/users/hahn/.bashrc
-source activate iq 
+conda activate iq 
 
 sim='simba'
-dem='slab_noll_m'
-#dem='slab_noll_msfr'
+dem='slab_noll_msfr'
 dist='L2'
 stat='3d'
 ################################################################################ 
-ofile="/home/users/hahn/projects/galpopFM/run/_siro/abc_"$sim"."$dem"."$dist"."$stat".o"
+# log
+################################################################################ 
+# 4/23/2020: running after implement instantaneous SFR=0 pre-sampling
+################################################################################ 
+ofile="/home/users/hahn/projects/galpopFM/run/_siro/_abc_"$sim"."$dem"."$dist"."$stat".o"
 >$ofile 
 
-mpiexec -n 8 python /home/users/hahn/projects/galpopFM/run/abc_siro.py \
-    $sim $dem $dist $stat $sim"."$dem"."$dist"."$stat 20 False 1000 \
+mpiexec -n 8 python /home/users/hahn/projects/galpopFM/run/run_abc.py \
+    siro $sim $dem $dist $stat $sim"."$dem"."$dist"."$stat 20 False 1000 \
     &>> $ofile
 ################################################################################ 
