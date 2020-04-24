128a9475947126987327d6ba0c03ca47f66d452c
changhoonhahn <changhoonhahn@lbl.gov>
2020-04-23 17:41:43 -0700

    running abc in mbp for testing purposes

---
+++ run/mbp_abc.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+source ~/.bash_profile
+conda activate iq 
+
+sim='simba'
+#dem='slab_noll_m'
+dem='slab_noll_msfr'
+dist='L2'
+stat='3d'
+################################################################################ 
+python /Users/ChangHoon/projects/galpopFM/run/run_abc.py \
+    mbp $sim $dem $dist $stat $sim"."$dem"."$dist"."$stat 3 False 10
+################################################################################ 
