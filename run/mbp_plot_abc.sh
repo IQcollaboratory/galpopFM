396b0402c54ba6b1bc1c47ea8e04888002e73dc0
changhoonhahn <changhoonhahn@lbl.gov>
2020-06-16 18:40:00 -0700

    gathered the latest ABC chains and updated resultsg

--- run/mbp_plot_abc.sh
+++ run/mbp_plot_abc.sh
@@ -2,15 +2,21 @@
 
 #dem=simba.slab_noll_msfr.L2.3d
 #dem=simba.tnorm_noll_msfr.L2.3d
+dem=simba.tnorm_noll_msfr_fixbump.L2.3d
+python /Users/ChangHoon/projects/galpopFM/run/plot_abc.py True $dem 6 6
 #dem=simba.slab_noll_msfr_fixbump.L2.3d
-#dem=simba.tnorm_noll_msfr_fixbump.L2.3d
+#python /Users/ChangHoon/projects/galpopFM/run/plot_abc.py True $dem 7 8
+
 #dem=tng.slab_noll_msfr.L2.3d
 #dem=tng.tnorm_noll_msfr.L2.3d
-dem=tng.slab_noll_msfr_fixbump.L2.3d
 #dem=tng.tnorm_noll_msfr_fixbump.L2.3d
+#python /Users/ChangHoon/projects/galpopFM/run/plot_abc.py True $dem 0 6
+#dem=tng.slab_noll_msfr_fixbump.L2.3d
+#python /Users/ChangHoon/projects/galpopFM/run/plot_abc.py True $dem 6 8
+
 #dem=eagle.slab_noll_msfr.L2.3d
 #dem=eagle.tnorm_noll_msfr.L2.3d
-#dem=eagle.slab_noll_msfr_fixbump.L2.3d
-#dem=eagle.tnorm_noll_msfr_fixbump.L2.3d
-
-python /Users/ChangHoon/projects/galpopFM/run/plot_abc.py True $dem 5 6
+dem=eagle.tnorm_noll_msfr_fixbump.L2.3d
+python /Users/ChangHoon/projects/galpopFM/run/plot_abc.py True $dem 6 6
+#dem=eagle.slab_noll_msfr_fixbump.L2.3d 
+#python /Users/ChangHoon/projects/galpopFM/run/plot_abc.py True $dem 9 11
