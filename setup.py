2647f34eae1ebdecf18b9ffae7a89ee1561fbed7
changhoonhahn <changh20@gmail.com>
2020-10-19 15:39:37 -0400

    profiling tng and other fm

--- setup.py
+++ setup.py
@@ -3,13 +3,21 @@ from setuptools import setup
 
 __version__ = '0.1'
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name = 'galpopfm',
-      version = __version__,
-      python_requires='>3.5.2',
-      description = 'forward modeling galaxy populations',
-      install_requires = ['numpy', 'matplotlib', 'h5py', 'abcpmc'], #'mpi4py', 
-      provides = ['galpopfm'],
-      packages = ['galpopfm'],
-      include_package_data=True, 
-      package_data={'galpopfm': ['dat/*.dat']}
-      )
+        version = __version__,
+        author="ChangHoon Hahn", 
+        author_email="hahn.changhoon@gmail.com", 
+        description = 'Python Package for forward modeling galaxy populations from simulations',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/IQcollaboratory/galpopFM/tree/master/galpopfm", 
+        packages=setuptools.find_packages(),
+        install_requires = ['numpy', 'matplotlib', 'h5py', 'abcpmc', 'corner'], #'mpi4py', 
+        provides = ['galpopfm'],
+        include_package_data=True, 
+        package_data={'galpopfm': ['dat/*.dat']},
+        python_requires='>3.6'
+        )
